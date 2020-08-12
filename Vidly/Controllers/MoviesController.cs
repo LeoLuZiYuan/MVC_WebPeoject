74a0afde8b1fc80832aa137dfc196f001cca7170
leolu <leolu@ddsc.com>
2020-08-12 18:17:45 +0800

    200812

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Vidly.Models;
+using String = System.String;
 
 namespace Vidly.Controllers
 {
@@ -12,9 +15,39 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movies = new Movie(){ Name = "Shrek!"};
+            var movies = new Movie() { Name = "Shrek!" };
 
-            return View(movies);
+            //return View(movies);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+        }
+
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        /// mvcaction4  action snipper
+        
+         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
